6f5fc9a634ff0b5405c83d13456bfb9feaf9df41
yenuy <yench0408@gmail.com>
2026-03-05 15:06:55 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .model_manager import ModelManager
-from .feature_generator import FeatureGenerator
-from .predictors import PredictorManager
-from .utils import validate_smiles
-
-__all__ = ['ModelManager', 'FeatureGenerator', 'PredictorManager', 'validate_smiles']
